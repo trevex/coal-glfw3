accaba8ff62c60a2d37f33b18fe902835918e3dd
Nik Voss <niklas.voss@gmail.com>
2015-12-10 15:53:59 +0100

    Added unziping and variables.

--- coalfile.py
+++ coalfile.py
@@ -3,5 +3,8 @@ from util import download, unzip
 
 class GLFW3File(CoalFile):
     version = "3.1.2"
+    zipfile = "glfw-%s.zip" % version
+    url = "https://github.com/glfw/glfw/releases/download/%s/%s" % (version, zipfile)
     def prepare(self):
-        download('https://github.com/glfw/glfw/releases/download/3.1.2/glfw-3.1.2.zip')
+        download(self.url, self.zipfile)
+        unzip(self.zipfile, 'src')
